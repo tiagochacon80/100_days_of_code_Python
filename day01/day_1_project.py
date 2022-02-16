9221fc1da7225568443fc3ece6151d2d70358ede
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-16 17:11:47 -0500

    first commit

---
+++ day01/day_1_project.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+
+city = input("Which city did you grow up in?\n ")#\n skip a line and the cursor goes to the bottom line
+
+pet = input("What is the name of a pet?\n ")
+
+print("Your band name could be" + city + " " + pet)
