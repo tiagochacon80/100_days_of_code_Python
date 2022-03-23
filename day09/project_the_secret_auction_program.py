8229e8ebbd231b0e26b15da5077af0fd0003859f
Tiago Chacon <tiagochacon80@gmail.com>
2022-03-23 15:53:58 -0400

    day 10 and update day 9

--- day09/project_the_secret_auction_program.py
+++ day09/project_the_secret_auction_program.py
@@ -3,9 +3,13 @@ print(logo)
 
 clearConsole = lambda: print('\n' * 50)
 
+bids = {}
+bidding_finished = False
+
 def find_highest_bidder(bidding_record):
     highest_bid = 0
     winner = ""
+    # bidding_record = {"Angela": 123, "James": 321}
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
@@ -13,19 +17,21 @@ def find_highest_bidder(bidding_record):
             winner = bidder
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-bids = {}
-bidding_finished = False
-
 while not bidding_finished:
     name = input("What is your name? ")
-    price = int(input("What is your bid? $ "))
+    price = int(input("What is your bid? $"))
     bids[name] = price
-    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    should_continue = input("Are there any other bidders? 'yes' or 'no' ")
     if should_continue == "no":
         bidding_finished = True
-        find_highest_bidder()
+        find_highest_bidder(bids)
     elif should_continue == "yes":
         clearConsole()
+
+
+
+
+
 
 
 
