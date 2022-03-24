5c7f4e6c27ef6e57a20feb776a01b5cbe3f2ed9a
Tiago Chacon <tiagochacon80@gmail.com>
2022-03-24 14:04:04 -0400

    update day 10

--- day10/calculator.py
+++ day10/calculator.py
@@ -13,34 +13,38 @@ def divide(n1, n2):
     return n1 / n2
 
 operations = {
-    "+": add, 
+    "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
 }
 
 def calculator():
-    print(logo)
 
     num1 = float(input("What's the first number: "))
-    for symbol in operations:
-        print(symbol)
+    for symbols in operations:
+        print(symbols)
+
     should_continue = True
-
     while should_continue:
-        operations_symbol = input("Pick an operation: ")
-        num2 = float(input("What's the next number? "))
-        calculation_function = operations[operations_symbol]
-        answer = calculation_function(num1, num2)
-        print(f"{num1} {operations_symbol} {num2} = {answer}")
+        operation_symbols = input("Pick the operation: ")
+        num2 = float(input("What's the next number: "))
+        function_operation = operations[operation_symbols]
+        answer = function_operation(num1, num2)
+        print(f"{num1} {operation_symbols} {num2} = {answer}")
 
-        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exist: ") == "y":
+        if print(f"Type 'y' for continue calculating with {answer}, or 'n' to restart. ") == 'y':
             num1 = answer
         else:
             should_continue = False
             calculator()
 
 calculator()
+
+
+
+
+
 
 
 
