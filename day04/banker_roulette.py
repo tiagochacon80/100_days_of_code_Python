ea80bf557a86874a8ca93f79f2d327b146ddfbfe
Tiago Chacon <tiagochacon80@gmail.com>
2022-03-27 16:12:13 -0400

    review day 4

--- day04/banker_roulette.py
+++ day04/banker_roulette.py
@@ -1,14 +1,16 @@
 from random import randint
 
 names = input("Give me everybody's names, separated by a coma.\n")
-names_separated = names.split(', ')
+
+names_separated = names.split(", ")
 
 compriment = len(names_separated)
 
-random_choice = randint(0, compriment - 1)
+random_choises = randint(0, compriment - 1)
 
-friend_chosen = names_separated[random_choice]
+friend_chosen = names_separated[random_choises]
 
-print((f"Who will pay de the bill? {friend_chosen}"))
+print(f"Who will pay the bill? {friend_chosen}")
+
 
 
