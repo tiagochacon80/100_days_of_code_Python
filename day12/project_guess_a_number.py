5ab7e40fd550bec83382c4a1d4577515a02bfea3
Tiago Chacon <tiagochacon80@gmail.com>
2022-03-29 11:35:29 -0400

    update day 12

--- day12/project_guess_a_number.py
+++ day12/project_guess_a_number.py
@@ -1,4 +1,15 @@
+from random import randint
+
+#Function to check user's guess against actual answer.
+def check_answer():
+
 #Choosing a random number between 1 and 100.
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1, 100)
 
 
 #Make function to set difficulty.
+
+#Let the user guess a number.
+guess_number = int(input("Make a guess: "))
