bc3e95fb9fc95a83205d818275b99c988bd0759a
Tiago Chacon <tiagochacon80@gmail.com>
2022-04-01 15:26:43 -0400

    day 15

--- day15/main.py
+++ day15/main.py
@@ -30,3 +30,9 @@ resources = {
     "coffee": 100,
 }
 
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
